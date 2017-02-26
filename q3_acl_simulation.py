94291b357bb31d39a1802ebe746dc0b33ac33b1e
庄园 <zhuangyuan@zhuangyuandeMacBook-Pro.local>
2017-02-26 16:59:04 -0400

    a

--- q3_acl_simulation.py
+++ q3_acl_simulation.py
@@ -13,26 +13,6 @@ def write_file(file_name,list):
         for line in list:
             f.write(' '.join(line))
 
-#return the protocl of the acl line
-def check_protocol(acl_line):
-    if len(acl_line) == 9:
-        if acl_line[3] == 'TCP':
-            if acl_line[8] == 'eq.20'or acl_line[8] == 'eq.21':
-                return 'FTP'
-            elif acl_line[8] == 'eq.22':
-                return 'SSH'
-            elif acl_line[8] == 'eq.23':
-                return 'Telnet'
-            elif acl_line[8] == 'eq.80':
-                return 'HTTP'
-        elif acl_line[3] == 'UDP':
-            if acl_line[8] == 'eq.161':
-                return 'SNMP'
-        elif acl_line[3] == 'IP':
-            return 'ANY'
-    else:
-        return 'ANY'
-
 #check each line of standard acl
 def check_ip_standard(acl_line,packet_line):
     packet_source_set = packet_line[0].split('.')#split source ip address of packet into 4 items list
@@ -47,8 +27,8 @@ def check_ip_standard(acl_line,packet_line):
     return True
 
 def check_ip_extended(acl_line,packet_line):
-    packet_souce_set = packet_line[1].split('.')
-    packet_dest_set = packet_line[2].split('.')
+    packet_souce_set = packet_line[0].split('.')
+    packet_dest_set = packet_line[1].split('.')
     #split source and destionation ip address of packet into 4 items list each
     source_ip_set = acl_line[4].split('.')
     source_mask_set = acl_line[5].split('.')
@@ -62,9 +42,10 @@ def check_ip_extended(acl_line,packet_line):
         if dest_mask_set[i] == '0':     #if 0 in mask, then check
             if dest_ip_set[i] != packet_dest_set[i]:
                 return False    #address does not match so return false
-        if check_protocol(acl_line) != 'ANY':#if not allow all protocols
-            if packet_line[0] != check_protocol(acl_line):
+        if len(acl_line)>=9:
+            if acl_line[8].split('.')[1] != packet_line[2]:
                 return False    #protocol does not match so return false
+    print(packet_line)
     return True
 
 
@@ -72,28 +53,30 @@ def ACL_operation(acl,packets):
     dict = {'deny':'Denied','permit':'permitted'}
     if len(acl[0])==5 :
         for item in packets: #check each packet
-            for line in acl[:-2]: #check if packet match line in acl
-                if len(item) < 3 and len(line) < 4:
-                    item.append('denied\n')
-                else:
-                    if check_ip_standard(line,item) == True:
-                        item.append('%s\n'%dict[line[2]])
-                        break
+            for line in acl: #check if packet match line in acl
+                if line[0] == 'access-list':
+                    if len(item) < 3 and len(line) < 4:
+                        item.append('denied\n')
                     else:
-                        continue
-                #if the address can not match all acls,then deny
+                        if check_ip_standard(line,item) == True:
+                            item.append('%s\n'%dict[line[2]])
+                            break
+                        else:
+                            continue
+                    #if the address can not match all acls,then deny
     if len(acl[0]) >= 8:
         for item in packets: #check each packet
-            for line in acl[:-2]:#check if packet match line in acl
-                if len(item) < 4 and len(line) <4:
-                    item.append('denied\n')
-                else:
-                    if check_ip_extended(line, item) == True:
-                        item.append('%s\n' % dict[line[2]])
-                        break
+            for line in acl:#check if packet match line in acl
+                if line[0] == 'access-list':
+                    if len(item) < 4 and len(line) <4:
+                        item.append('denied\n')
                     else:
-                        continue
-                # if the address can not match all acls,then deny
+                        if check_ip_extended(line, item) == True:
+                            item.append('%s\n' % dict[line[2]])
+                            break
+                        else:
+                            continue
+                    # if the address can not match all acls,then deny
     return packets
 
 
@@ -107,4 +90,3 @@ if __name__ == '__main__':
     write_file('Packets_out',ACL_operation(acl,packets))
     write_file('Exteneded_Packets_out', ACL_operation(extended_acl, extended_packets))
     # call operation functions
-    print("acl operation down")
